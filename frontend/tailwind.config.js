ee33184c971212183a612d1a13d7ce3fc0b020ac
you112ef <abu822204@gmail.com>
2025-11-02 16:58:32 +0300

    Add frontend/tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          900: '#0f172a',
+          800: '#1e293b',
+          700: '#334155',
+          600: '#475569',
+          500: '#64748b',
+          400: '#94a3b8',
+          300: '#cbd5e1',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
